259f324b1516dc902c234d9aee32914c2b33fb8d
sanoueze777 <79368786+sanoueze777@users.noreply.github.com>
2023-06-01 13:48:44 +0200

    Add files via upload

--- plan_test.py
+++ plan_test.py
@@ -14,7 +14,7 @@ from sqlite3 import Error
 from datetime import datetime
 import mysql.connector
 from pandasql import sqldf
-
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
 ##########################################  deployed
 """ """""""""
@@ -414,7 +414,25 @@ if authentication_status:
         st.altair_chart(chart_3, use_container_width=True)
         
         st.dataframe(experts_table.style.applymap(color_survived, subset=["taux d'exécution %"]))
-        
+
+        gb = GridOptionsBuilder.from_dataframe(data)
+	gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+	gb.configure_side_bar() #Add a sidebar
+	gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+	gridOptions = gb.build()
+
+	grid_response = AgGrid(
+    		experts_table,
+    		gridOptions=gridOptions,
+    		data_return_mode='AS_INPUT', 
+    		update_mode='MODEL_CHANGED', 
+    		fit_columns_on_grid_load=False,
+    		theme='blue', #Add theme color to the table
+   		enable_enterprise_modules=True,
+    		height=350, 
+    		width='100%',
+    		reload_data=True
+		)
         
         
     if choice == "Suivi des missions":
